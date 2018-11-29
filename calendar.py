99c990510a31870b6cb463e1663749a619fa8cd1
Andrew Everitt <everitta@allegheny.edu>
2018-11-29 14:20:19 -0500

    Commit 8: Interactivity almost complete

--- calendar.py
+++ calendar.py
@@ -5,7 +5,7 @@
 ## TODO: 1) Main function connect to account 2) Create event class 3) Create calculator function to find difference between dates 4) Clean up code & write comments
 
 def main():
-    response = input(" Welcome to the ACE Calendar! Please enter your UserID to access your account, type 'new' to make a new account, or 'quit' to exit: ")
+    response = input(" Welcome to the ACE Calendar! Please enter your UserID to access your account, type 'new' to make a new account, 'list' to see all accounts, or 'quit' to exit: ")
     if response.upper() == "NEW":
         response2 = input(" Thanks for choosing ACE Calendar. Enter the UserID you would like to use: ")
         print(" Welcome ", response2)
@@ -21,6 +21,10 @@ def main():
         else:
             print(" The answer you provided is not 'yes' or 'no.' Returning to the main menu...")
             main()
+    elif response.upper() == "LIST":
+        print(" Here are all of the accounts: ")
+        #print account list (name only)
+        main()
     elif response.upper() == "QUIT":
         quit()
     else:
@@ -28,54 +32,75 @@ def main():
         interact()
 # end of main function
 
+
 def interact():
     action = input("What action would you like to take today? Enter '1' to view event list, '2' to view priority list, '3' to add an additional event, '4' to delete an event, or '5' to switch users: ")
-    if action == "1":
+    if action == "1": #event list by date
         answer = input(" Enter '1' to see your events, '2' to see global events, or '3' to see all events: ")
-        if answer == "1":
+        if answer == "1": #showing personal events
             print(" Your events, based on date, are: ")
-        elif answer == "2":
+            #code to print events
+            ret = input(" Please type 'back' when you are ready to return to the action screen: ")
+            if ret.upper() == "BACK":
+                interact()
+            else:
+                print(" Response unknown, returning to the action screen... ")
+                interact()
+        elif answer == "2": #showing global events
             print(" The global events, based on date, are: ")
-        elif answer == "3":
+            #code to print events
+            ret = input(" Please type 'back' when you are ready to return to the action screen: ")
+            if ret.upper() == "BACK":
+                interact()
+            else:
+                print(" Response unknown, returning to the action screen... ")
+                interact()
+        elif answer == "3": #showing personal and global events
             print(" The list of your events and the global ones, based on date, is as follows: ")
             ret = input(" Please type 'back' when you are ready to return to the action screen: ")
             if ret.upper() == "BACK":
                 interact()
             else:
-                print(" Please type 'back' when you are ready to return to the action screen: ")
+                print(" Response unknown, returning to the action screen... ")
+                interact()
         else:
-            print(" The number you entered isn't an available option. Returning to the action screen...")
+            print(" The number you entered isn't an available option. Returning to the main menu...")
             main()
-    elif action == "2":
+
+    elif action == "2": #event list by priority
         answer = input(" Enter '1' to see the priority of your events, '2' to see the priority of global events, '3' to see the priority of all events, or '4' to go back: ")
-        if answer == "1":
+        if answer == "1": #priority of personal events
             print(" Your events, from highest to lowest priority, are: ")
             ret = input(" Please type 'back' when you are ready to return to the action screen: ")
             if ret.upper() == "BACK":
                 interact()
             else:
-                print(" Please type 'back' when you are ready to return to the action screen: ")
-        elif answer == "2":
+                print(" Response unknown, returning to the action screen... ")
+                interact()
+        elif answer == "2": #priority of global events
             print(" The global events, from highest to lowest priority, are: ")
             ret = input(" Please type 'back' when you are ready to return to the action screen: ")
             if ret.upper() == "BACK":
                 interact()
             else:
-                print(" Please type 'back' when you are ready to return to the action screen: ")
-        elif answer == "3":
-            print(" The list of your events and the global ones, from highest to lowest priority, is as follows: ")
+                print(" Response unknown, returning to the action screen... ")
+                interact()
+        elif answer == "3": #priority of personal and global events
+            print(" The list of your events and the global ones, from highest to lowest priority, is as follows: ") #if a global and personal event has the same priority, the event closest to the date is placed higher. If same date, global placed first.
             ret = input(" Please type 'back' when you are ready to return to the action screen: ")
             if ret.upper() == "BACK":
                 interact()
             else:
-                print(" Please type 'back' when you are ready to return to the action screen: ")
+                print(" Response unknown, returning to the action screen... ")
+                interact()
         elif answer == "4":
             print(" Returning to the action screen...")
             interact()
         else:
             print(" The number you entered isn't an available option. Returning to the action screen...")
             interact()
-    elif action == "3":
+
+    elif action == "3": #creating a new event
         answer = input(" Please enter 'global' to create a global event, 'private' for an event that can only be seen by you, or type 'back' to go back: ")
         if answer.upper() == "GLOBAL":
             answer2 = input(" Please enter the name of the global event you want to add or type 'back' to go back: ")
@@ -83,17 +108,50 @@ def interact():
                 print(" Returning to the action screen...")
                 interact()
             else:
-                answer3 = input(" Now enter the date as one, eight-digit number. For example, if the event is January 1, 2019, you would enter 01012019 (MMDDYYYY):") #need to add check for past dates
+                answer3 = int(input(" Now enter the date as one, eight-digit number. For example, if the event is January 1, 2019, you would enter 01012019 (MMDDYYYY):")) #need to add check for past dates
+                getToday() #returns todays date
+                if today > answer3:
+                    print(" The date you have entered has already passed. Returning to action screen... ")
+                    interact()
+                else:
+                    answer4 = input(" Next, enter the name of your event: ")
+                    answer5 = int(input(" Finally, enter the priority of this event (1 being the highest priority and please note that if an event with the designated priority exists, it will be moved to a lower priority): "))
+                    #check for existing priority, changes priorities if needed
+                    #create event object with date, name, and priority
         elif answer.upper() == "PRIVATE":
             answer2 = input(" Please enter the name of the private event you want to add or type 'back' to go back: ")
             if answer2.upper() == "BACK":
                 print(" Returning to the action screen...")
                 interact()
+            else:
+                answer3 = int(input(" Now enter the date as one, eight-digit number. For example, if the event is January 1, 2019, you would enter 01012019 (MMDDYYYY):")) #need to add check for past dates
+                getToday() #returns todays date
+                if today > answer3:
+                    print(" The date you have entered has already passed. Returning to action screen... ")
+                    interact()
+                answer4 = input(" Next, enter the name of your event: ")
+                answer5 = int(input(" Finally, enter the priority of this event (1 being the highest priority and please note that if an event with the designated priority exists, it will be moved to a lower priority): "))
+                #check for existing priority, changes priorities if needed
+                #create event object with date, name, and priority
         else:
             print(" Returning to the action screen...")
             interact()
-    elif action == "4":
+
+    elif action == "4": #deleting an event
         answer = input(" Please enter the name of the event you want to delete or type 'back' to go back: ")
+        if answer.back() == "BACK":
+            print(" Returning to the action screen... ")
+            interact()
+        else:
+            #checks to see if event with name exists
+            answer2 = input(" Are you sure you want to delete the following event? Type 'yes to confirm: '")
+            if answer2.upper() == "YES":
+                #delete the event and change priorities
+                print(" The event has been successfully deleted. Returning to the action screen... ")
+                interact()
+            else:
+                print(" The event has not been deleted. Returning to the action screen... ")
+                interact()
     elif action == "5":
         main()
     else:
@@ -124,35 +182,33 @@ class Event:
         #self.event = event #string
         # Extract the first and last names
         name_pieces = full_name.split(" ") #ret a list
-        self.first_name = name_pieces[0] # first element
-        self.last_name = name_pieces[1]  # second element
     #end of __init__()
 #end of class
 
 
-    def getToday(self):
-        """returns today's data in yyyy-mm-dd format"""
-        import datetime #library
-        today = datetime.datetime.today().strftime('%Y-%m-%d') # On one line. Is a string
-        yyyy = int(today[0:4])
-        mm = int(today[5:7])
-        dd = int(today[8:10])
-        today = datetime.date(yyyy,mm,dd) #date of birth
-        return today
-        #end of getToday()
+def getToday(self):
+    """returns today's data in yyyy-mm-dd format"""
+    import datetime #library
+    today = datetime.datetime.today().strftime('%Y-%m-%d') # On one line. Is a string
+    yyyy = int(today[0:4])
+    mm = int(today[5:7])
+    dd = int(today[8:10])
+    today = datetime.date(yyyy,mm,dd) #date of birth
+    return today
+    #end of getToday()
 
-    def ageMethod(self):
-        """Return the age of the person in years. Convert birthday to get these years."""
-        yyyy_b = int(self.birthday[0:4])
-        mm_b = int(self.birthday[4:6])
-        dd_b = int(self.birthday[6:8])
-         #date of birth
-        dob = datetime.date(yyyy_b,mm_b,dd_b)
-        today = self.getToday()
-        age_in_days = (today - dob).days
-        age_in_years = age_in_days/365
-        return int(age_in_years)
-        #end of age()
+def ageMethod(self):
+    """Return the age of the person in years. Convert birthday to get these years."""
+    yyyy_b = int(self.birthday[0:4])
+    mm_b = int(self.birthday[4:6])
+    dd_b = int(self.birthday[6:8])
+     #date of birth
+    dob = datetime.date(yyyy_b,mm_b,dd_b)
+    today = self.getToday()
+    age_in_days = (today - dob).days
+    age_in_years = age_in_days/365
+    return int(age_in_years)
+    #end of age()
 
 import datetime # library
 
